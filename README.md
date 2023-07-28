cc1dd3f406b05bbfc3c934f4538cb6cc9be14858
tainamiranda2 <tainaferreira0987@gmail.com>
2023-07-28 10:37:35 -0300

    migrantion ok

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 ## instalação 
+### tenha ao menos php 7.4 e composer 2 instalado
+
 ### composer create-project --prefer-dist cakephp/app:~4.0 my_app_name
 
 ## rodar o servidor
 ### cake server php - S localhost:8000
+
+## conexao com banco de dados
+### config - Datasources, nome do banco, senha e banco em si
+## migration - tabela no banco
+###  bin/cake bake  migration Users
+### criando campos:  bin/cake  migrations migrate
+
+##
+###
---
+++ config/Migrations/20230728131408_Users.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class Users extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change():void
+    {
+     $table=   $this->table( 'users');
+    //adicionando colunas
+     $table->addColumn('firstnName', 'string', [
+        'limit'=>100,
+        'null'=>false,
+
+    ]);
+    $table->addColumn('lastName', 'string', [
+        'limit'=>100,
+        'null'=>true,
+
+    ]);
+   
+
+    $table->addColumn('email', 'string', [
+        'limit'=>255,
+        'null'=>false,
+
+    ]);
+    
+    $table->addColumn('password', 'string', [
+        'limit'=>255,
+        'null'=>false,
+
+    ]);
+//horario que criar o valor do campo
+    $table->addColumn('created_at', 'timestamp', [
+        'default'=>'CURRENT_TIMESTAMP',
+    ]);
+
+    //horario que atualizar o valor do campo
+
+    $table->addColumn('update_at', 'timestamp', [
+        'default'=>'CURRENT_TIMESTAMP',
+        'update'=> 'CURRENT_TIMESTAMP'
+    ]);
+
+     //deixando o email unico
+     $table->addIndex('email', ['unique'=>true]);
+     //criando a tabela
+     $table->create();
+     
+    }
+}
